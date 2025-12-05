9f41b27563f8b783b093d1e67c665267caae8e16
yurimg-11 <yologm@hotmail.com>
2025-12-05 13:33:13 -0600

    first commit

---
+++ src/app/prueba/prueba.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba',
+  imports: [],
+  templateUrl: './prueba.component.html',
+  styleUrl: './prueba.component.scss'
+})
+export class PruebaComponent {
+
+}
